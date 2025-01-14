e0e52859983d85af1a0ad2c4d6cab175f8d3fe42
david <mrminecraft00@gmail.com>
2025-01-14 22:09:46 +0100

    Refactor action types and serialization handling.
    Updated action types to explicitly define associated classes and improved serialization by registering additional adapters. Streamlined imports using wildcards for better readability and maintenance.

--- src/main/java/net/thenextlvl/character/plugin/CharacterPlugin.java
+++ src/main/java/net/thenextlvl/character/plugin/CharacterPlugin.java
@@ -23,21 +23,16 @@ import net.thenextlvl.character.CharacterController;
 import net.thenextlvl.character.CharacterProvider;
 import net.thenextlvl.character.PlayerCharacter;
 import net.thenextlvl.character.action.ActionType;
+import net.thenextlvl.character.action.ClickAction;
+import net.thenextlvl.character.plugin.character.PaperCharacterController;
+import net.thenextlvl.character.plugin.character.PaperCharacterProvider;
 import net.thenextlvl.character.plugin.character.action.PaperActionType;
 import net.thenextlvl.character.plugin.command.CharacterCommand;
-import net.thenextlvl.character.plugin.character.PaperCharacterController;
 import net.thenextlvl.character.plugin.listener.CharacterListener;
 import net.thenextlvl.character.plugin.listener.ConnectionListener;
 import net.thenextlvl.character.plugin.listener.EntityListener;
 import net.thenextlvl.character.plugin.listener.test;
-import net.thenextlvl.character.plugin.character.PaperCharacterProvider;
-import net.thenextlvl.character.plugin.serialization.CharacterSerializer;
-import net.thenextlvl.character.plugin.serialization.ComponentAdapter;
-import net.thenextlvl.character.plugin.serialization.EntityTypeAdapter;
-import net.thenextlvl.character.plugin.serialization.KeyAdapter;
-import net.thenextlvl.character.plugin.serialization.LocationAdapter;
-import net.thenextlvl.character.plugin.serialization.ProfilePropertyAdapter;
-import net.thenextlvl.character.plugin.serialization.WorldAdapter;
+import net.thenextlvl.character.plugin.serialization.*;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.Location;
 import org.bukkit.World;
@@ -73,12 +68,18 @@ public class CharacterPlugin extends JavaPlugin {
     private final File translations = new File(getDataFolder(), "translations");
 
     private final NBT nbt = NBT.builder()
+            .registerTypeHierarchyAdapter(ActionType.class, new ActionTypeAdapter(this))
+            .registerTypeHierarchyAdapter(InetSocketAddress.class, new AddressAdapter())
             .registerTypeHierarchyAdapter(Character.class, new CharacterSerializer())
+            .registerTypeHierarchyAdapter(ClickAction.class, new ClickActionAdapter())
             .registerTypeHierarchyAdapter(Component.class, new ComponentAdapter())
             .registerTypeHierarchyAdapter(EntityType.class, new EntityTypeAdapter())
             .registerTypeHierarchyAdapter(Key.class, new KeyAdapter())
             .registerTypeHierarchyAdapter(Location.class, new LocationAdapter())
             .registerTypeHierarchyAdapter(ProfileProperty.class, new ProfilePropertyAdapter())
+            .registerTypeHierarchyAdapter(Sound.class, new SoundAdapter())
+            .registerTypeHierarchyAdapter(Title.Times.class, new TitleTimesAdapter())
+            .registerTypeHierarchyAdapter(Title.class, new TitleAdapter())
             .registerTypeHierarchyAdapter(World.class, new WorldAdapter(getServer()))
             .build();
     private final PaperCharacterController characterController = new PaperCharacterController(this);
@@ -232,20 +233,22 @@ public class CharacterPlugin extends JavaPlugin {
         root.optional("invincible").map(Tag::getAsBoolean).ifPresent(character::setInvincible);
         root.optional("pose").map(Tag::getAsString).map(Pose::valueOf).ifPresent(character::setPose);
         root.optional("visibleByDefault").map(Tag::getAsBoolean).ifPresent(character::setVisibleByDefault);
+        root.optional("clickActions").map(Tag::getAsCompound).ifPresent(actions -> actions.forEach((name, action) ->
+                character.addAction(name, nbt.fromTag(action, ClickAction.class))));
         return character;
     }
 
-    public final ActionType<Component> sendActionbar = register(new PaperActionType<>("send_actionbar", Audience::sendActionBar));
-    public final ActionType<Component> sendMessage = register(new PaperActionType<Component>("send_message", Audience::sendMessage));
-    public final ActionType<InetSocketAddress> transfer = register(new PaperActionType<>("transfer",
+    public final ActionType<Component> sendActionbar = register(new PaperActionType<>("send_actionbar", Component.class, Audience::sendActionBar));
+    public final ActionType<Component> sendMessage = register(new PaperActionType<>("send_message", Component.class, Audience::sendMessage));
+    public final ActionType<InetSocketAddress> transfer = register(new PaperActionType<>("transfer", InetSocketAddress.class,
             (player, address) -> player.transfer(address.getHostName(), address.getPort())));
-    public final ActionType<Location> teleport = (register(new PaperActionType<>("teleport", Entity::teleportAsync)));
-    public final ActionType<Sound> playSound = register(new PaperActionType<>("play_sound", Audience::playSound));
-    public final ActionType<String> connect = register(new PaperActionType<>("connect", messenger::connect));
-    public final ActionType<String> runConsoleCommand = register(new PaperActionType<>("run_console_command", (player, command) ->
+    public final ActionType<Location> teleport = (register(new PaperActionType<>("teleport", Location.class, Entity::teleportAsync)));
+    public final ActionType<Sound> playSound = register(new PaperActionType<>("play_sound", Sound.class, Audience::playSound));
+    public final ActionType<String> connect = register(new PaperActionType<>("connect", String.class, messenger::connect));
+    public final ActionType<String> runConsoleCommand = register(new PaperActionType<>("run_console_command", String.class, (player, command) ->
             player.getServer().dispatchCommand(player.getServer().getConsoleSender(), command)));
-    public final ActionType<String> runCommand = register(new PaperActionType<>("run_command", Player::performCommand));
-    public final ActionType<Title> sendTitle = register(new PaperActionType<>("send_title", Audience::showTitle));
+    public final ActionType<String> runCommand = register(new PaperActionType<>("run_command", String.class, Player::performCommand));
+    public final ActionType<Title> sendTitle = register(new PaperActionType<>("send_title", Title.class, Audience::showTitle));
 
     private <T> ActionType<T> register(ActionType<T> actionType) {
         return characterProvider().getActionRegistry().register(actionType);
