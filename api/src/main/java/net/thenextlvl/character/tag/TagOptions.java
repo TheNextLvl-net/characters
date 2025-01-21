2fc0ed90d7541f170f7726261e0b22c54715c450
david <mrminecraft00@gmail.com>
2025-01-21 17:35:13 +0100

    Refactor TagOptions interface and add nullability annotations
    Refactored TagOptions to implement TagSerializable and updated method signatures for clarity and consistency. Added @NullMarked for enhanced null-safety. Introduced new methods for managing Billboard properties.

--- api/src/main/java/net/thenextlvl/character/tag/TagOptions.java
+++ api/src/main/java/net/thenextlvl/character/tag/TagOptions.java
@@ -1,8 +1,17 @@
 package net.thenextlvl.character.tag;
 
-import org.bukkit.entity.TextDisplay;
+import core.nbt.serialization.TagSerializable;
+import org.bukkit.entity.Display.Billboard;
+import org.bukkit.entity.TextDisplay.TextAlignment;
+import org.jspecify.annotations.NullMarked;
 
-public interface TagOptions {
-    TextDisplay.TextAlignment getAlignment();
-    boolean setAlignment(TextDisplay.TextAlignment alignment);
+@NullMarked
+public interface TagOptions extends TagSerializable {
+    Billboard getBillboard();
+
+    TextAlignment getAlignment();
+
+    boolean setAlignment(TextAlignment alignment);
+
+    boolean setBillboard(Billboard billboard);
 }
