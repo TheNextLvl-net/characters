8fcd141a2282336968980cc3bcee8acc37c642ab
david <mrminecraft00@gmail.com>
2025-01-27 23:10:19 +0100

    Remove unused AttributeProvider from CharacterProvider interface
    The `attributeProvider()` method was removed as it is no longer in use. This simplifies the `CharacterProvider` interface and eliminates unnecessary dependencies.

--- api/src/main/java/net/thenextlvl/character/CharacterProvider.java
+++ api/src/main/java/net/thenextlvl/character/CharacterProvider.java
@@ -1,15 +1,12 @@
 package net.thenextlvl.character;
 
 import net.thenextlvl.character.action.ActionTypeProvider;
-import net.thenextlvl.character.attribute.AttributeProvider;
 import net.thenextlvl.character.skin.SkinFactory;
 import org.jspecify.annotations.NullMarked;
 
 @NullMarked
 public interface CharacterProvider {
     ActionTypeProvider actionTypeProvider();
-
-    AttributeProvider attributeProvider();
 
     CharacterController characterController();
 
