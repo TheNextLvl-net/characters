1f3bbe7b147b1cfb6980b0ca9ad35a0af4ece66d
david <mrminecraft00@gmail.com>
2025-01-27 23:08:18 +0100

    Refactor `AttributeType` to support entities and key-based design.
    Reworked `AttributeType` to include entity type bindings with `Key`, enabling better modularity and extensibility. Introduced getters and setters for attributes and updated equality and hash methods based on `Key`. This improves usability for managing attributes tied to specific entities.

--- api/src/main/java/net/thenextlvl/character/attribute/AttributeType.java
+++ api/src/main/java/net/thenextlvl/character/attribute/AttributeType.java
@@ -1,43 +1,75 @@
 package net.thenextlvl.character.attribute;
 
+import net.kyori.adventure.key.Key;
+import net.kyori.adventure.key.KeyPattern;
+import net.kyori.adventure.key.Keyed;
+import org.bukkit.entity.Entity;
 import org.jspecify.annotations.NonNull;
 
 import java.util.Objects;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
 
-public class AttributeType<T> {
+public class AttributeType<E extends Entity, T> implements Keyed {
+    private final @NonNull BiConsumer<@NonNull E, T> setter;
+    private final @NonNull Class<@NonNull E> entityType;
     private final @NonNull Class<T> dataType;
-    private final @NonNull String name;
+    private final @NonNull Function<@NonNull E, T> getter;
+    private final @NonNull Key key;
 
-    public AttributeType(@NonNull String name, @NonNull Class<T> dataType) {
-        this.dataType = dataType;
-        this.name = name;
+    public AttributeType(@KeyPattern String key, @NonNull Class<@NonNull E> entityType, @NonNull Class<T> dataType,
+                         @NonNull Function<@NonNull E, T> getter, @NonNull BiConsumer<@NonNull E, T> setter) {
+        this(Key.key(key), entityType, dataType, getter, setter);
     }
 
-    public @NonNull Class<T> getDataType() {
+    public AttributeType(@NonNull Key key, @NonNull Class<@NonNull E> entityType, @NonNull Class<T> dataType,
+                         @NonNull Function<@NonNull E, T> getter, @NonNull BiConsumer<@NonNull E, T> setter) {
+        this.dataType = dataType;
+        this.entityType = entityType;
+        this.getter = getter;
+        this.key = key;
+        this.setter = setter;
+    }
+
+    public void set(E entity, T value) {
+        setter.accept(entity, value);
+    }
+
+    public T get(E entity) {
+        return getter.apply(entity);
+    }
+
+    public @NonNull Class<E> entityType() {
+        return entityType;
+    }
+
+    public @NonNull Class<T> dataType() {
         return dataType;
     }
 
-    public @NonNull String getName() {
-        return name;
+    @Override
+    public @NonNull Key key() {
+        return key;
     }
 
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
-        AttributeType<?> that = (AttributeType<?>) o;
-        return Objects.equals(dataType, that.dataType) && Objects.equals(name, that.name);
+        AttributeType<?, ?> that = (AttributeType<?, ?>) o;
+        return Objects.equals(key, that.key);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(dataType, name);
+        return Objects.hash(key);
     }
 
     @Override
-    public String toString() {
+    public @NonNull String toString() {
         return "AttributeType{" +
-               "dataType=" + dataType +
-               ", name='" + name + '\'' +
+               "entityType=" + entityType +
+               ", dataType=" + dataType +
+               ", key=" + key +
                '}';
     }
 }
