965902f2a6dbb68be642733e2340a177df7edc0a
david <github@nonswag.dev>
2025-03-26 14:42:37 +0100

    Update Paperweight plugin to version 2.0.0-beta.16
    Upgraded the Paperweight userdev plugin from beta.14 to beta.16 to ensure compatibility with the latest features and fixes. This change keeps the build system up to date with recommended versions.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("java")
     id("com.gradleup.shadow") version "9.0.0-beta9"
     id("io.papermc.hangar-publish-plugin") version "0.1.2"
-    id("io.papermc.paperweight.userdev") version "2.0.0-beta.14"
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.16"
     id("de.eldoria.plugin-yml.paper") version "0.7.1"
 }
 
