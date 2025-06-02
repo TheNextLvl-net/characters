7705b6e217d2b9b4c49eec4409377e45fa311ccc
david <github@nonswag.dev>
2025-06-02 16:27:31 +0200

    Integrated Modrinth publishing plugin
    Added `com.modrinth.minotaur` plugin to facilitate automatic publishing to Modrinth and configured necessary settings such as project ID, token, and game versions.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ import net.minecrell.pluginyml.paper.PaperPluginDescription
 plugins {
     id("java")
     id("com.gradleup.shadow") version "9.0.0-beta15"
+    id("com.modrinth.minotaur") version "2.+"
     id("io.papermc.hangar-publish-plugin") version "0.1.3"
     id("io.papermc.paperweight.userdev") version "2.0.0-beta.17"
     id("de.eldoria.plugin-yml.paper") version "0.7.1"
@@ -133,4 +134,13 @@ hangarPublish { // docs - https://docs.papermc.io/misc/hangar-publishing
             platformVersions.set(versions)
         }
     }
+}
+
+modrinth {
+    token.set(System.getenv("MODRINTH_TOKEN"))
+    projectId.set("oeBa7ep1")
+    versionType = if (isRelease) "release" else "beta"
+    uploadFile.set(tasks.shadowJar)
+    gameVersions.set(versions)
+    loaders.add("paper")
 }
