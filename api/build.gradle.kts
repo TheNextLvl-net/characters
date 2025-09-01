b007d0d656c748bd9dabb4792658a6503fc33674
david <github@nonswag.dev>
2025-09-01 22:09:19 +0200

    Added snapshots repository and new dependencies
    Included the `net.thenextlvl.core:paper:2.3.0-pre2` dependency and configured the snapshots repository in `build.gradle.kts` files.

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -27,6 +27,7 @@ repositories {
 
 dependencies {
     api("net.thenextlvl:nbt:3.0.0-pre1")
+    api("net.thenextlvl.core:paper:2.3.0-pre2")
     compileOnly("io.papermc.paper:paper-api:1.21.8-R0.1-SNAPSHOT")
 }
 
