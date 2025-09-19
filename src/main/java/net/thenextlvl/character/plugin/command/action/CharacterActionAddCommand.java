07091b99770b04ad3250e49db8ecf7723745d5dc
david <github@nonswag.dev>
2025-09-19 11:21:22 +0200

    Refactor CharacterActionAddCommand methods
    Simplify command method signatures by removing ClickTypes parameter
    and updating related method calls for improved readability and maintainability.

--- src/main/java/net/thenextlvl/character/plugin/command/action/CharacterActionAddCommand.java
+++ src/main/java/net/thenextlvl/character/plugin/command/action/CharacterActionAddCommand.java
@@ -13,6 +13,7 @@ import core.paper.command.argument.codec.EnumStringCodec;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
 import io.papermc.paper.command.brigadier.argument.ArgumentTypes;
+import io.papermc.paper.command.brigadier.argument.RegistryArgumentExtractor;
 import io.papermc.paper.command.brigadier.argument.resolvers.FinePositionResolver;
 import io.papermc.paper.command.brigadier.argument.resolvers.RotationResolver;
 import io.papermc.paper.math.Rotation;
@@ -31,7 +32,6 @@ import net.thenextlvl.character.plugin.CharacterPlugin;
 import net.thenextlvl.character.plugin.command.brigadier.BrigadierCommand;
 import net.thenextlvl.character.plugin.model.ClickTypes;
 import org.bukkit.EntityEffect;
-import org.bukkit.Registry;
 import org.bukkit.World;
 import org.jspecify.annotations.NullMarked;
 import org.jspecify.annotations.Nullable;
@@ -52,191 +52,171 @@ final class CharacterActionAddCommand extends BrigadierCommand {
     static LiteralArgumentBuilder<CommandSourceStack> create(CharacterPlugin plugin) {
         var command = new CharacterActionAddCommand(plugin);
         var tree = command.create();
-        for (var clickTypes : ClickTypes.values()) {
-            var literal = clickTypes.name().toLowerCase();
-            var chain = Commands.literal(literal)
-                    .then(command.connect(clickTypes))
-                    .then(command.playSound(clickTypes))
-                    .then(command.runConsoleCommand(clickTypes))
-                    .then(command.runPlayerCommand(clickTypes))
-                    .then(command.sendActionBar(clickTypes))
-                    .then(command.sendEntityEffect(clickTypes))
-                    .then(command.sendMessage(clickTypes))
-                    .then(command.teleport(clickTypes))
-                    .then(command.title(clickTypes))
-                    .then(command.transfer(clickTypes));
-            tree.then(characterArgument(plugin).then(actionArgument(plugin).then(chain)));
-        }
+        var chain = clickTypesArgument()
+                .then(command.connect())
+                .then(command.playSound())
+                .then(command.runConsoleCommand())
+                .then(command.runPlayerCommand())
+                .then(command.sendActionBar())
+                .then(command.sendEntityEffect())
+                .then(command.sendMessage())
+                .then(command.teleport())
+                .then(command.title())
+                .then(command.transfer());
+        tree.then(characterArgument(plugin).then(actionArgument(plugin).then(chain)));
         return tree;
     }
 
-    private ArgumentBuilder<CommandSourceStack, ?> title(ClickTypes clickTypes) {
+    private ArgumentBuilder<CommandSourceStack, ?> title() {
         return Commands.literal("send-title")
                 .then(Commands.argument("title", StringArgumentType.string())
                         .then(Commands.argument("subtitle", StringArgumentType.string())
-                                .then(titleTimesArgument(clickTypes))
+                                .then(titleTimesArgument())
                                 .executes(context -> {
                                     var subtitle = MiniMessage.miniMessage().deserialize(
                                             context.getArgument("subtitle", String.class));
-                                    return title(context, subtitle, null, clickTypes);
+                                    return title(context, subtitle, null);
                                 }))
-                        .executes(context -> title(context, Component.empty(), null, clickTypes)));
+                        .executes(context -> title(context, Component.empty(), null)));
     }
 
-    private ArgumentBuilder<CommandSourceStack, ?> titleTimesArgument(ClickTypes clickTypes) {
+    private ArgumentBuilder<CommandSourceStack, ?> titleTimesArgument() {
         return Commands.argument("fade-in", ArgumentTypes.time())
                 .then(Commands.argument("stay", ArgumentTypes.time())
                         .then(Commands.argument("fade-out", ArgumentTypes.time())
-                                .executes(context -> title(context, clickTypes))));
+                                .executes(this::title)));
     }
 
-    private ArgumentBuilder<CommandSourceStack, ?> connect(ClickTypes clickTypes) {
-        return Commands.literal("connect").then(stringArgument("server", ActionTypes.types().connect(), clickTypes));
+    private ArgumentBuilder<CommandSourceStack, ?> connect() {
+        return Commands.literal("connect").then(stringArgument("server", ActionTypes.types().connect()));
     }
 
-    private ArgumentBuilder<CommandSourceStack, ?> playSound(ClickTypes clickTypes) {
-        return Commands.literal("play-sound").then(soundArgument(plugin)
-                .then(soundSourceArgument(plugin)
+    private ArgumentBuilder<CommandSourceStack, ?> playSound() {
+        return Commands.literal("play-sound").then(soundArgument()
+                .then(soundSourceArgument()
                         .then(Commands.argument("volume", FloatArgumentType.floatArg(0))
                                 .then(Commands.argument("pitch", FloatArgumentType.floatArg(0, 2))
-                                        .executes(context -> {
-                                            var volume = context.getArgument("volume", float.class);
-                                            var pitch = context.getArgument("pitch", float.class);
-                                            return playSound(context, getSource(context), volume, pitch, clickTypes);
-                                        }))
-                                .executes(context -> {
-                                    var volume = context.getArgument("volume", float.class);
-                                    return playSound(context, getSource(context), volume, 1, clickTypes);
-                                }))
-                        .executes(context -> playSound(context, getSource(context), 1, 1, clickTypes)))
-                .executes(context -> playSound(context, Sound.Source.MASTER, 1, 1, clickTypes)));
+                                        .executes(this::playSound))
+                                .executes(this::playSound))
+                        .executes(this::playSound))
+                .executes(this::playSound));
     }
 
-    private ArgumentBuilder<CommandSourceStack, ?> runConsoleCommand(ClickTypes clickTypes) {
-        return Commands.literal("run-console-command").then(stringArgument("command", ActionTypes.types().runConsoleCommand(), clickTypes));
+    private ArgumentBuilder<CommandSourceStack, ?> runConsoleCommand() {
+        return Commands.literal("run-console-command").then(stringArgument("command", ActionTypes.types().runConsoleCommand()));
     }
 
-    private ArgumentBuilder<CommandSourceStack, ?> runPlayerCommand(ClickTypes clickTypes) {
-        return Commands.literal("run-command").then(stringArgument("command", ActionTypes.types().runCommand(), clickTypes));
+    private ArgumentBuilder<CommandSourceStack, ?> runPlayerCommand() {
+        return Commands.literal("run-command").then(stringArgument("command", ActionTypes.types().runCommand()));
     }
 
-    private ArgumentBuilder<CommandSourceStack, ?> sendActionBar(ClickTypes clickTypes) {
-        return Commands.literal("send-actionbar").then(stringArgument("message", ActionTypes.types().sendActionbar(), clickTypes));
+    private ArgumentBuilder<CommandSourceStack, ?> sendActionBar() {
+        return Commands.literal("send-actionbar").then(stringArgument("message", ActionTypes.types().sendActionbar()));
     }
 
-    private ArgumentBuilder<CommandSourceStack, ?> sendEntityEffect(ClickTypes clickTypes) {
-        return Commands.literal("send-entity-effect").then(entityEffectArgument(plugin).executes(context -> {
+    private ArgumentBuilder<CommandSourceStack, ?> sendEntityEffect() {
+        return Commands.literal("send-entity-effect").then(entityEffectArgument().executes(context -> {
             var entityEffect = context.getArgument("entity-effect", EntityEffect.class);
-            return addAction(context, ActionTypes.types().sendEntityEffect(), entityEffect, clickTypes);
+            return addAction(context, ActionTypes.types().sendEntityEffect(), entityEffect);
         }));
     }
 
-    private ArgumentBuilder<CommandSourceStack, ?> sendMessage(ClickTypes clickTypes) {
-        return Commands.literal("send-message").then(stringArgument("message", ActionTypes.types().sendMessage(), clickTypes));
+    private ArgumentBuilder<CommandSourceStack, ?> sendMessage() {
+        return Commands.literal("send-message").then(stringArgument("message", ActionTypes.types().sendMessage()));
     }
 
-    private ArgumentBuilder<CommandSourceStack, ?> teleport(ClickTypes clickTypes) {
-        return Commands.literal("teleport").then(positionArgument(plugin)
+    private ArgumentBuilder<CommandSourceStack, ?> teleport() {
+        return Commands.literal("teleport").then(positionArgument()
                 .then(Commands.argument("rotation", ArgumentTypes.rotation())
                         .then(Commands.argument("world", ArgumentTypes.world())
-                                .executes(context -> {
-                                    var rotation = context.getArgument("rotation", RotationResolver.class);
-                                    var world = context.getArgument("world", World.class);
-                                    return teleport(context, rotation.resolve(context.getSource()), world, clickTypes);
-                                }))
-                        .executes(context -> {
-                            var rotation = context.getArgument("rotation", RotationResolver.class);
-                            var world = context.getSource().getLocation().getWorld();
-                            return teleport(context, rotation.resolve(context.getSource()), world, clickTypes);
-                        }))
-                .executes(context -> {
-                    var world = context.getSource().getLocation().getWorld();
-                    return teleport(context, Rotation.rotation(0, 0), world, clickTypes);
-                }));
+                                .executes(this::teleport))
+                        .executes(this::teleport))
+                .executes(this::teleport));
     }
 
-    private ArgumentBuilder<CommandSourceStack, ?> transfer(ClickTypes clickTypes) {
+    private ArgumentBuilder<CommandSourceStack, ?> transfer() {
         return Commands.literal("transfer").then(Commands.argument("hostname", StringArgumentType.string())
                 .then(Commands.argument("port", IntegerArgumentType.integer(1, 65535))
                         .executes(context -> {
                             var port = context.getArgument("port", int.class);
-                            return transfer(context, port, clickTypes);
+                            return transfer(context, port);
                         }))
-                .executes(context -> transfer(context, 25565, clickTypes)));
+                .executes(context -> transfer(context, 25565)));
     }
 
-    private ArgumentBuilder<CommandSourceStack, ?> stringArgument(String name, ActionType<String> actionType, ClickTypes clickTypes) {
+    private ArgumentBuilder<CommandSourceStack, ?> stringArgument(String name, ActionType<String> actionType) {
         return Commands.argument(name, StringArgumentType.greedyString())
                 .executes(context -> {
                     var string = context.getArgument(name, String.class);
-                    return addAction(context, actionType, string, clickTypes);
+                    return addAction(context, actionType, string);
                 });
     }
 
-    private int playSound(CommandContext<CommandSourceStack> context, Sound.Source source, float volume, float pitch, ClickTypes clickTypes) {
-        var sender = context.getSource().getSender();
-        var sound = context.getArgument("sound", org.bukkit.Sound.class);
-        var key = Registry.SOUNDS.getKey(sound);
-        if (key == null) throw new NullPointerException("Unknown sound");
-        return addAction(context, ActionTypes.types().playSound(), Sound.sound(key, source, volume, pitch), clickTypes);
-    }
+    private int teleport(CommandContext<CommandSourceStack> context) throws CommandSyntaxException {
+        var position = context.getArgument("position", FinePositionResolver.class).resolve(context.getSource());
+        var rotationResolver = tryGetArgument(context, "rotation", RotationResolver.class).orElse(null);
 
-    private int teleport(CommandContext<CommandSourceStack> context, Rotation rotation, World world, ClickTypes clickTypes) throws CommandSyntaxException {
-        var resolver = context.getArgument("position", FinePositionResolver.class);
-        var position = resolver.resolve(context.getSource());
+        var world = tryGetArgument(context, "world", World.class).orElseGet(() -> context.getSource().getLocation().getWorld());
+        var rotation = rotationResolver != null ? rotationResolver.resolve(context.getSource()) : Rotation.rotation(0, 0);
+
         var location = position.toLocation(world).setRotation(rotation);
-        return addAction(context, ActionTypes.types().teleport(), location, clickTypes);
+        return addAction(context, ActionTypes.types().teleport(), location);
     }
 
-    private int title(CommandContext<CommandSourceStack> context, ClickTypes clickTypes) {
+    private int title(CommandContext<CommandSourceStack> context) {
         var subtitle = MiniMessage.miniMessage().deserialize(
                 context.getArgument("subtitle", String.class));
         var fadeIn = Ticks.duration(context.getArgument("fade-in", int.class));
         var stay = Ticks.duration(context.getArgument("stay", int.class));
         var fadeOut = Ticks.duration(context.getArgument("fade-out", int.class));
         var times = Title.Times.times(fadeIn, stay, fadeOut);
-        return title(context, subtitle, times, clickTypes);
+        return title(context, subtitle, times);
     }
 
-    private int title(CommandContext<CommandSourceStack> context, Component subtitle, Title.@Nullable Times times, ClickTypes clickTypes) {
+    private int title(CommandContext<CommandSourceStack> context, Component subtitle, Title.@Nullable Times times) {
         var title = MiniMessage.miniMessage().deserialize(context.getArgument("title", String.class));
-        return addAction(context, ActionTypes.types().sendTitle(), Title.title(title, subtitle, times), clickTypes);
+        return addAction(context, ActionTypes.types().sendTitle(), Title.title(title, subtitle, times));
     }
 
-    private int transfer(CommandContext<CommandSourceStack> context, int port, ClickTypes clickTypes) {
+    private int transfer(CommandContext<CommandSourceStack> context, int port) {
         var hostname = context.getArgument("hostname", String.class);
         var address = new InetSocketAddress(hostname, port);
-        return addAction(context, ActionTypes.types().transfer(), address, clickTypes);
+        return addAction(context, ActionTypes.types().transfer(), address);
     }
 
-    private ArgumentBuilder<CommandSourceStack, ?> clickTypesArgument(CharacterPlugin plugin) {
+    private static ArgumentBuilder<CommandSourceStack, ?> clickTypesArgument() {
         return Commands.argument("click-types", EnumArgumentType.of(ClickTypes.class, EnumStringCodec.lowerHyphen()));
     }
 
-    private ArgumentBuilder<CommandSourceStack, ?> entityEffectArgument(CharacterPlugin plugin) {
+    private static ArgumentBuilder<CommandSourceStack, ?> entityEffectArgument() {
         return Commands.argument("entity-effect", EnumArgumentType.of(EntityEffect.class, EnumStringCodec.lowerHyphen()));
     }
 
-    private ArgumentBuilder<CommandSourceStack, ?> positionArgument(CharacterPlugin plugin) {
+    private static ArgumentBuilder<CommandSourceStack, ?> positionArgument() {
         return Commands.argument("position", ArgumentTypes.finePosition());
     }
 
-    private ArgumentBuilder<CommandSourceStack, ?> soundArgument(CharacterPlugin plugin) {
-        return Commands.argument("sound", ArgumentTypes.resource(RegistryKey.SOUND_EVENT));
+    private static ArgumentBuilder<CommandSourceStack, ?> soundArgument() {
+        return Commands.argument("sound", ArgumentTypes.resourceKey(RegistryKey.SOUND_EVENT));
     }
 
-    private ArgumentBuilder<CommandSourceStack, ?> soundSourceArgument(CharacterPlugin plugin) {
+    private static ArgumentBuilder<CommandSourceStack, ?> soundSourceArgument() {
         return Commands.argument("sound-source", EnumArgumentType.of(Sound.Source.class, EnumStringCodec.lowerHyphen()));
     }
 
-    private Sound.Source getSource(CommandContext<CommandSourceStack> context) {
-        return context.getArgument("sound-source", Sound.Source.class);
+    private int playSound(CommandContext<CommandSourceStack> context) {
+        var source = tryGetArgument(context, "source", Sound.Source.class).orElse(Sound.Source.MASTER);
+        var volume = tryGetArgument(context, "volume", float.class).orElse(1f);
+        var pitch = tryGetArgument(context, "pitch", float.class).orElse(1f);
+        var sound = RegistryArgumentExtractor.getTypedKey(context, RegistryKey.SOUND_EVENT, "sound");
+        return addAction(context, ActionTypes.types().playSound(), Sound.sound(sound, source, volume, pitch));
     }
 
-    private <T> int addAction(CommandContext<CommandSourceStack> context, ActionType<T> actionType, T input, ClickTypes clickTypes) {
+    private <T> int addAction(CommandContext<CommandSourceStack> context, ActionType<T> actionType, T input) {
         var sender = context.getSource().getSender();
         var character = (Character<?>) context.getArgument("character", Character.class);
         var actionName = context.getArgument("action", String.class);
+        var clickTypes = context.getArgument("click-types", ClickTypes.class);
 
         var previous = character.getAction(actionName);
         var cooldown = previous.map(ClickAction::getCooldown).orElse(Duration.ZERO);
