3bd539ee195904a12a24153fbb6b17397ea992ba
david <github@nonswag.dev>
2025-09-19 14:43:29 +0200

    Add character equipment clear and set commands
    Implement `CharacterEquipmentClearCommand` and `CharacterEquipmentSetCommand` to manage character equipment. Refactor `CharacterEquipmentCommand` to integrate these new commands.

---
+++ src/main/java/net/thenextlvl/character/plugin/command/equipment/CharacterEquipmentCommand.java
@@ -0,0 +1,30 @@
+package net.thenextlvl.character.plugin.command.equipment;
+
+import com.mojang.brigadier.builder.ArgumentBuilder;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import core.paper.command.argument.EnumArgumentType;
+import core.paper.command.argument.codec.EnumStringCodec;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import io.papermc.paper.command.brigadier.Commands;
+import net.thenextlvl.character.plugin.CharacterPlugin;
+import net.thenextlvl.character.plugin.command.brigadier.BrigadierCommand;
+import org.bukkit.inventory.EquipmentSlot;
+import org.jspecify.annotations.NullMarked;
+
+@NullMarked
+public final class CharacterEquipmentCommand extends BrigadierCommand {
+    private CharacterEquipmentCommand(CharacterPlugin plugin) {
+        super(plugin, "equipment", "characters.command.equipment");
+    }
+
+    public static LiteralArgumentBuilder<CommandSourceStack> create(CharacterPlugin plugin) {
+        var command = new CharacterEquipmentCommand(plugin);
+        return command.create()
+                .then(CharacterEquipmentClearCommand.create(plugin))
+                .then(CharacterEquipmentSetCommand.create(plugin));
+    }
+
+    static ArgumentBuilder<CommandSourceStack, ?> equipmentSlotArgument() {
+        return Commands.argument("equipment-slot", EnumArgumentType.of(EquipmentSlot.class, EnumStringCodec.lowerHyphen()));
+    }
+}
